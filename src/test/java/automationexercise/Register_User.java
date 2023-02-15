0839896dd1fb85f601a9d5985de594cc963f4e8f
erhan paker <erhanpaker@gmail.com>
2023-02-15 15:10:35 +0300

    automationexercise.com

---
+++ src/test/java/automationexercise/Register_User.java
@@ -0,0 +1,50 @@
+package automationexercise;
+
+import org.junit.Assert;
+import org.junit.Test;
+import utilities.TestBaseMethod;
+
+public class Register_User extends TestBaseMethod {
+
+    // 1. Launch browser
+    //2. Navigate to url 'http://automationexercise.com'
+    @Test
+
+    public void test1 (){
+        driver.get("http://automationexercise.com");
+
+
+
+    }
+    //3. Verify that home page is visible successfully
+    @Test
+
+    public void görünürOlduğumuTestEtme(){
+
+        String expectedTitle="automationexercise";
+
+        String  actualUrl = driver.getCurrentUrl();
+        Assert.assertFalse(actualUrl.contains(expectedTitle));
+    }
+
+
+
+
+    //4. Click on 'Signup / Login' button
+    //5. Verify 'New User Signup!' is visible
+    //6. Enter name and email address
+    //7. Click 'Signup' button
+    //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+    //9. Fill details: Title, Name, Email, Password, Date of birth
+    //10. Select checkbox 'Sign up for our newsletter!'
+    //11. Select checkbox 'Receive special offers from our partners!'
+    //12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+    //13. Click 'Create Account button'
+    //14. Verify that 'ACCOUNT CREATED!' is visible
+    //15. Click 'Continue' button
+    //16. Verify that 'Logged in as username' is visible
+    //17. Click 'Delete Account' button
+    //18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+
+
+}
